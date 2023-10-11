5e66cecbe0263d70cb60ba54b4846929ef77a96b
KevinUJ <kevin.useche@pirani.co>
2023-10-10 21:42:57 -0500

    delete secret key

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,7 +1,6 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'admin123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
