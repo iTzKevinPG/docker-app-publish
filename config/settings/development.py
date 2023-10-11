72022490efb48f379577318634cbdeccf176f734
KevinUJ <kevin.useche@pirani.co>
2023-10-10 22:31:49 -0500

    add secret key

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,6 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'admin123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- config/settings/production.py
+++ config/settings/production.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv(Path.joinpath(BASE_DIR, '.env'))
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
